86b30f2348cfbefbc8ac60a4af1f558da291f5b7
Matthew <matthew_mcaleer@hotmail.com>
2022-02-22 22:53:23 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!'
+  } else if (distance > 2500) {
+    return 'No can do.'
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+} 
+
+function ternaryCheckCity(city) {
+  let answer
+  city === 'NYC' ? (answer = 'Ok, sounds good.') : (answer = 'No go.');
+  return answer;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
